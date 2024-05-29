1af4802d5fcb5ca3363e470a1228b3b1e402a3cc
RZTecx <ezequielclara2@gmail.com>
2024-05-29 18:44:38 -0300

    se agrega el proyecto

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let copy = document.querySelector(".media__slider").cloneNode(true);
+document.querySelector(".media").appendChild(copy);
+
+// const piloto = document.getElementById("piloto");
+// const remolcador = document.getElementById("remolcador");
+//  const mainImage = document.getElementById("main-image");
+
+// piloto.addEventListener('click', () => {
+//    mainImage.src = "assets/img/roadmap-instructor.webp";
+// });
